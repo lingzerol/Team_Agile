3001a0c19748f793523a5e16b63b087e30e178b6
Carmen <654813926@qq.com>
2019-05-05 21:00:59 +0800

    update

--- Team_Agile/Properties/AssemblyInfo.cs
+++ Team_Agile/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("fe18f32a-5d3c-4f86-a7c8-7208acfa7a80")]
+[assembly: Guid("9c3ddb93-a790-429b-aeb0-ecb3eafb53f1")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+// 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
