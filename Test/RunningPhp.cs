96351e78e440543aca325986cf5c104bf0a8f3e7
lingzerol <467264565@qq.com>
2019-05-05 23:23:46 +0800

    run php

---
+++ Test/RunningPhp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team_Agile;
+namespace Test
+{
+    [TestClass]
+    public class RunningPhp
+    {
+        [TestMethod]
+        public void RunningPhpCgiTest()
+        {
+            Php php = new Php();
+            string request = "hello world!!!";
+            string result=php.Excute("echo \"hello world!!!\";");
+            Assert.AreEqual(result, request);
+        }
+    }
+}
