7848190fb7ceb1aecbe8826f6a056eca32b60cfa
Carmen <654813926@qq.com>
2019-05-06 17:55:06 +0800

    http-post-response测试代码

--- Lib/HttpPost.cs
+++ Lib/HttpPost.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lib
 {
-    class HttpPost
+    public class HttpPost
     {
+        private string _who;
+
+        public HttpPost(string who) {
+            this._who = who;
+        }
+
+        public string Excute(string command)
+        {
+            return this._who;
+            //待补充
+        }
     }
 }
