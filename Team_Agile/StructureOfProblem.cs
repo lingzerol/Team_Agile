3147da2f74696322057981c698504e1ebbfbf3db
CrysisXX <39306002+CrysisXX@users.noreply.github.com>
2019-05-11 22:55:32 +0800

    添加问题数据结构定义

---
+++ Team_Agile/StructureOfProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Agile
+{
+    class StructureOfProblem
+    {
+        // 问题数据结构
+        public string QuestionName;
+        public string TimeLimitIndex;
+        public string MemoryLimitIndex;
+        public string TotalSubmits;
+        public string TotalAccepts;
+        public string AcceptsRate;
+        public string QuestionDescription;
+        public string InputDescription;
+        public string OutputDescription;
+        public string InputSample;
+        public string OutputSample;
+        public string Hint;
+    }
+}
