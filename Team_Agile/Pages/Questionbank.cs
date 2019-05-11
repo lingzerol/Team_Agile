f7e894d920d750a48ee398e684cc94609f2f219d
CrysisXX <705066853@qq.com>
2019-05-12 00:18:57 +0800

    题集数据结构和窗体

---
+++ Team_Agile/Pages/Questionbank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team_Agile.Pages
+{
+    public partial class Questionbank : Form
+    {
+        public Questionbank()
+        {
+            InitializeComponent();
+            TableLoad();
+        }
+
+        private void TableLoad()
+        {
+            this.show_question.GridLines = true;
+            this.show_question.View = View.Details;
+            this.show_question.Scrollable = true;
+
+            this.show_question.Columns.Add("Pro.ID", 70, HorizontalAlignment.Center);
+            this.show_question.Columns.Add("Problem Title", 300, HorizontalAlignment.Center);
+            this.show_question.Columns.Add("Ratio(Accepted/Submissions)", 150, HorizontalAlignment.Center);
+        }
+    }
+}
