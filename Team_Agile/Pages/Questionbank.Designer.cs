31fc892113d3e0406648f6b059790d58b81c4940
镇 张 <zhen183653821@outlook.com>
2019-05-19 09:46:11 +0800

    change windows size

--- Team_Agile/Pages/Questionbank.Designer.cs
+++ Team_Agile/Pages/Questionbank.Designer.cs
@@ -36,10 +36,9 @@
             // 
             // show_question
             // 
-            this.show_question.Location = new System.Drawing.Point(14, 80);
-            this.show_question.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.show_question.Location = new System.Drawing.Point(12, 67);
             this.show_question.Name = "show_question";
-            this.show_question.Size = new System.Drawing.Size(965, 568);
+            this.show_question.Size = new System.Drawing.Size(1155, 580);
             this.show_question.TabIndex = 0;
             this.show_question.UseCompatibleStateImageBehavior = false;
             // 
@@ -47,17 +46,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(403, 23);
+            this.label1.Location = new System.Drawing.Point(502, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 52);
+            this.label1.Size = new System.Drawing.Size(156, 45);
             this.label1.TabIndex = 1;
             this.label1.Text = "PHP题集";
             // 
             // test
             // 
-            this.test.Location = new System.Drawing.Point(592, 35);
+            this.test.Location = new System.Drawing.Point(683, 29);
+            this.test.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.test.Name = "test";
-            this.test.Size = new System.Drawing.Size(100, 28);
+            this.test.Size = new System.Drawing.Size(89, 25);
             this.test.TabIndex = 2;
             this.test.TextChanged += new System.EventHandler(this.Test_TextChanged);
             // 
@@ -65,9 +65,10 @@
             // 
             this.confirm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.confirm.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.confirm.Location = new System.Drawing.Point(710, 37);
+            this.confirm.Location = new System.Drawing.Point(795, 29);
+            this.confirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.confirm.Name = "confirm";
-            this.confirm.Size = new System.Drawing.Size(64, 25);
+            this.confirm.Size = new System.Drawing.Size(78, 25);
             this.confirm.TabIndex = 3;
             this.confirm.Text = "确定";
             this.confirm.UseVisualStyleBackColor = true;
@@ -75,14 +76,13 @@
             // 
             // Questionbank
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 664);
+            this.ClientSize = new System.Drawing.Size(1179, 702);
             this.Controls.Add(this.confirm);
             this.Controls.Add(this.test);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.show_question);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Questionbank";
             this.Text = "Questionbank";
             this.Load += new System.EventHandler(this.Questionbank_Load);
